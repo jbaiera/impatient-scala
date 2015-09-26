b281f32cc35d567403ce14610c0ff40311af9159
David Kwasny <dkwasny67@gmail.com>
2015-09-25 21:34:59 -0400

    Kwas implementation

--- src/main/scala/list/Driver.scala
+++ src/main/scala/list/Driver.scala
@@ -1,9 +1,60 @@
+package list
+
 object Driver extends App {
+
+	def addByX(x: Int): (Int) => Int = _ + x
+	val addOne = addByX(1)
+	def runTwice(f: (Int) => Int): (Int) => Int = in => f(f(in))
+		
+	println("1 through 6")
+	var list = LinkedList()
+	list = list += 1 += 2 += 3 += 4 += 5 += 6
+	println(list)
+
+	println("empty list")
+	var emptyList = new LinkedList()
+	println(emptyList)
+
+	println("multiplied")
+	val multiplyByTwo = (i: Int) => { i * 2 }
+	var multiplied = list.map(multiplyByTwo)
+	println(multiplied)
 	
-	//Create list
+	println("add one")
+	var added = list.map(addOne)
+	println(added)
 
-	//Add x elements
+	println("add one and multiply")
+	var addedAndMultiplied = list.map(addOne).map(multiplyByTwo)
+	println(addedAndMultiplied)
 
-	//print the list
+	println("add by x=10")
+	var addByXEqualsTen = list.map(addByX(10))
+	println(addByXEqualsTen)
 
+	println("add twice")
+	var addTwice = list.map(runTwice(addByX(1)))
+	println(addTwice)
+
+	println("group by even")
+	var groupByEven = list.groupBy(_ % 2 == 0)
+	println(groupByEven)
+
+	println("Reduce")
+	//var reduce = list.reduce((in1, in2) => in1 * in2)
+	var reduce = list.reduce(_ * _)
+	println(reduce)
+
+	println("Curry")
+	val curry = (x: Int) => (y: Int) => x * y
+	val curry2 = curry(2)
+	val curry24 = curry2(4)
+	println(curry24)	
+	
+	println("Hot Curry")
+	def hotCurry(x: Int)(y: Int)(z: Int) = x * y + z
+	val hotCurry2 = hotCurry(2)(_)
+	val hotCurry24 = hotCurry2(4)(_)
+	val hotCurry243 = hotCurry24(3)
+	println(hotCurry243)
 }
--- src/main/scala/list/LinkedList.scala
+++ src/main/scala/list/LinkedList.scala
@@ -1,13 +1,62 @@
+package list
+
+import list.LinkedList.Node
+import list.LinkedList.LinkedIterator
+
 object LinkedList {
-	class Node(var data: Int, var next: Node) {}
+	class Node(var data: Int, var next: Option[Node] = Option.empty) {}
+	class LinkedIterator(var node: Option[Node]) extends Iterator[Int] {
+		def hasNext(): Boolean = {
+			node.isDefined
+		}
+		def next(): Int = {
+			val r = node.get.data
+			node = node.get.next
+			r
+		}
+	}
+	
+	def apply(): LinkedList = {
+		new LinkedList()
+	}
 }
 
-class LinkedList private(var size: Int, private var start: Option[LinkedList.Node] = Option.empty) {
-	
-	private[this] class Node(var data: Int, var next: Node) {}
+class LinkedList private(private var innerSize: Int, private var start: Option[Node] = Option.empty, private var end: Option[Node] = Option.empty) extends Iterable[Int] {
 	
 	def this() {
 		this(0)
 	}
 	
+	def length = size
+
+	def add(value: Int): Unit = {
+		if (start.isEmpty) {
+			start = Option(new Node(value))
+			end = start
+		}
+		else {
+			var oldEnd = end
+			end = Option(new Node(value))
+			oldEnd.get.next = end
+		}
+		innerSize += 1
+	}
+
+	def +=(values: Iterable[Int]): LinkedList = {
+		for (i <- values) add(i)
+		this
+	}
+	
+	def +=(values: Int*): LinkedList = {
+		this += values
+	}
+
+	def apply(index: Int): Int = {
+		if (index < 0 || index >= innerSize) {
+			throw new RuntimeException("Bad Index " + index);
+		}
+		take(index).last
+	}
+
+	override def iterator(): LinkedIterator = new LinkedIterator(start)
 }
