3320f76a73fb1b99627379db894607fba8696c63
James Baiera <james.baiera@gmail.com>
2016-02-21 21:14:04 -0500

    Just throwing up a single exercise for tonight.

---
+++ src/main/scala/chapters/ch08/Ex01.scala
@@ -0,0 +1,21 @@
+package chapters.ch08
+
+/**
+  * Chapter 08, Example 01:
+  * Extend the following BankAccount class to a CheckingAccount class that charges $1
+  * for every deposit and withdrawal.
+  */
+object Ex01 extends App {
+
+  class BankAccount(initialBalance: Double) {
+    private var balance = initialBalance
+    def deposit(amount: Double) = { balance += amount; balance }
+    def withdraw(amount: Double) = { balance -= amount; balance }
+  }
+
+  class CheckingAccount(initialBalance: Double) extends BankAccount(initialBalance) {
+    override def deposit(amount: Double): Double = { super.deposit(amount); super.withdraw(1) }
+    override def withdraw(amount: Double): Double = { super.withdraw(amount); super.withdraw(1) }
+  }
+
+}
