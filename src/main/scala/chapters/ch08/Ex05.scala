6e0fd29c67cb111c1bc1b623dd5f7dc7fae88524
James Baiera <james.baiera@gmail.com>
2016-02-27 21:30:57 -0500

    Chapter 8 Exercise 5

--- src/main/scala/chapters/ch08/Ex05.scala
+++ src/main/scala/chapters/ch08/Ex05.scala
@@ -10,4 +10,8 @@ package chapters.ch08
   */
 object Ex05 extends App {
 
+  class Point(val x: Double, val y: Double)
+
+  class LabeledPoint(val label: String, x: Double, y: Double) extends Point(x, y)
+
 }
