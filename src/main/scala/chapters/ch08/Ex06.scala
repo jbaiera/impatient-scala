6fd7402c833ef007b92a556238027e38c0f671b7
James Baiera <james.baiera@gmail.com>
2016-02-28 22:30:54 -0500

    Chapter 8 Exercise 6

--- src/main/scala/chapters/ch08/Ex06.scala
+++ src/main/scala/chapters/ch08/Ex06.scala
@@ -1,5 +1,7 @@
 package chapters.ch08
 
+import chapters.ch08.Ex05.Point
+
 /**
   * Chapter 08, Example 06:
   * Define an abstract class Shape with an abstract method centerPoint and subclasses
@@ -8,4 +10,13 @@ package chapters.ch08
   */
 object Ex06 extends App {
 
+  abstract class Shape {
+    def centerPoint: Point
+  }
+
+  class Rectangle(val x: Double, val y: Double, val w: Double, val h: Double) extends Shape {
+    override def centerPoint = new Point(x + (w/2), y + (h/2))
+  }
+
+  class Circle(val centerPoint: Point, val radius: Double) extends Shape
 }
