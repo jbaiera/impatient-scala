98dadc812968292ff2e37958d53eea5d0706c77f
James Baiera <james.baiera@gmail.com>
2016-02-29 19:21:24 -0500

    Chapter 8 Exercise 7

--- src/main/scala/chapters/ch08/Ex07.scala
+++ src/main/scala/chapters/ch08/Ex07.scala
@@ -9,4 +9,14 @@ package chapters.ch08
   */
 object Ex07 extends App {
 
+  class Square(x: Int, y: Int, w: Int) extends java.awt.Rectangle(x, y, w, w) {
+    def this(w: Int) {
+      this(0,0,w)
+    }
+
+    def this() {
+      this(0,0,0)
+    }
+  }
+
 }
