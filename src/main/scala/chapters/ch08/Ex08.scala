b926a35feb3556c781763d9cc7e6788052c7df86
James Baiera <james.baiera@gmail.com>
2016-03-01 22:22:19 -0500

    Chapter 8 Exercise 8

--- src/main/scala/chapters/ch08/Ex08.scala
+++ src/main/scala/chapters/ch08/Ex08.scala
@@ -8,5 +8,23 @@ package chapters.ch08
   * Use the -c and -private options.)
   */
 object Ex08 extends App {
+  println(
+    """Answer:
+      |
+      |Person has a single private final field "name" and a single getter "name()"
+      |
+      |SecretAgent has a single private final field "name" and a single getter "name()"
+      |
+      |An interesting thing found though is that SecretAgent has a "toString" method that
+      |returns a field called "toString"
+      |""".stripMargin)
+}
 
+class Person(val name: String) {
+  override def toString = getClass.getName + "[name=" + name + "]"
+}
+
+class SecretAgent(codename: String) extends Person(codename) {
+  override val name = "secret"
+  override val toString = "secret"
 }
