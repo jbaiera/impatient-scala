a5efcbdb7e28800d531ad8dc9b27ba398ffc3574
James Baiera <james.baiera@gmail.com>
2016-03-05 00:11:44 -0500

    Chapter 8 Exercise 9

--- src/main/scala/chapters/ch08/Ex09.scala
+++ src/main/scala/chapters/ch08/Ex09.scala
@@ -8,5 +8,61 @@ package chapters.ch08
   * Why?
   */
 object Ex09 extends App {
+  val originalAnt = new original.Ant
+  val defAnt = new bothDef.Ant
+  val valAnt = new defAndVal.Ant
 
+  /*
+   * Set to zero because Creature calls range() which is a getter on Ant, a getter that refers to an
+   * uninitialized field. When the getter is executed, it returns the default value of an uninitialized Int, zero.
+   */
+  assert(originalAnt.env.length == 0)
+
+  /*
+   * Set to 2 because Creature calls range() which is a function that simply returns the constant 2. There is no
+   * internal field created that may be uninitialized. This is different because there is no field to be initialized.
+   * The function just returns a constant.
+   */
+  assert(defAnt.env.length == 2)
+
+  /*
+   * Set to 0 because Creature calls the range() function, which is overridden by the Ant class with a getter method
+   * that points to an uninitialized field. This is similar to case 1, in that there is a field on the subclass
+   * that is returned before initialization.
+   */
+  assert(valAnt.env.length == 0)
+
+}
+
+package original {
+  class Creature {
+    val range: Int = 10 // <- Both a getter method and a private final field.
+    val env: Array[Int] = new Array[Int](range) // <- calls range() on Ant, which returns uninitialized field.
+  }
+
+  class Ant extends Creature {
+    override val range = 2 // <- Both a getter method and a private final field.
+  }
+}
+
+package bothDef {
+  class Creature {
+    def range: Int = 10 // <- Simply a method that returns a constant value.
+    val env: Array[Int] = new Array[Int](range) // <- calls range() on Ant, which returns the constant "2"
+  }
+
+  class Ant extends Creature {
+    override def range = 2 // <- Simply a method that returns a constant value.
+  }
+}
+
+package defAndVal {
+  class Creature {
+    def range: Int = 10 // <- Simply a method that returns a constant value.
+    val env: Array[Int] = new Array[Int](range) // <- calls range() on Ant, which returns uninitialized field.
+  }
+
+  class Ant extends Creature {
+    override val range = 2 // <- Both a getter method and a private final field.
+  }
 }
