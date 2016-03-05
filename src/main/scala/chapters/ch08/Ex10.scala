8935cdc45c352bbc21d717b2a96a39b4a82aade0
James Baiera <james.baiera@gmail.com>
2016-03-04 23:55:08 -0500

    Chapter 8 Exercise 10

--- src/main/scala/chapters/ch08/Ex10.scala
+++ src/main/scala/chapters/ch08/Ex10.scala
@@ -3,10 +3,19 @@ package chapters.ch08
 /**
   * Chapter 08, Example 10:
   * The file scala/collection/immutable/Stack.scala contains the definition
-  * class Stack[A] protected (protected val elems: List[A])
-  * Explain the meanings of the protectedkeywords. (Hint: Review the discussion
+  *
+  *   class Stack[A] protected (protected val elems: List[A])
+  *
+  * Explain the meanings of the protected keywords. (Hint: Review the discussion
   * of private constructors in Chapter 5.)
   */
 object Ex10 extends App {
+
+  println(
+    """|Constructors can take a modifier like 'private' as a qualifier for
+       |what scope the constructor may be accessed from. The 'protected'
+       |keyword is used to grant access to a field or method only to
+       |a scope that belongs to a subclass of the owner of the field or method.
+    """.stripMargin)
 
 }
