dca8846c2887fb880fdd2004ad9d5c3cc0e9e957
James Baiera <james.baiera@gmail.com>
2016-03-07 20:28:22 -0500

    Chapter 9 Exercise 1

--- src/main/scala/chapters/ch09/Ex01.scala
+++ src/main/scala/chapters/ch09/Ex01.scala
@@ -1,5 +1,7 @@
 package chapters.ch09
 
+import scala.io.Source
+
 /**
   * Chapter 09, Example 01:
   * Write a Scala code snippet that reverses the lines in a file (making the last
@@ -7,4 +9,10 @@ package chapters.ch09
   */
 object Ex01 extends App {
 
+  val reversed = Source
+    .fromFile(getClass.getResource("/chapters/ch09/text.txt").toURI)
+    .getLines()
+    .foldLeft(Seq[String]())((l, v) => v +: l)
+
+  println(reversed.mkString("\n"))
 }
