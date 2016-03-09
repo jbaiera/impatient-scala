99239dd116e3f3ba630905033fddf0c765bc8b33
James Baiera <james.baiera@gmail.com>
2016-03-08 23:53:44 -0500

    Chapter 9 Exercise 3

--- src/main/scala/chapters/ch09/Ex03.scala
+++ src/main/scala/chapters/ch09/Ex03.scala
@@ -1,10 +1,20 @@
 package chapters.ch09
 
+import chapters.ch09.Ex01._
+
+import scala.io.Source
+
 /**
   * Chapter 09, Example 03:
   * Write a Scala code snippet that reads a file and prints all words with more
   * than 12 characters to the console. Extra credit if you can do this in a single line.
   */
 object Ex03 extends App {
-
+  Source
+    .fromFile(getClass.getResource("/chapters/ch09/text.txt").toURI)
+    .getLines()
+    .toSeq
+    .flatMap(_.split(' '))
+    .filter(_.length > 12)
+    .foreach(println(_))
 }
