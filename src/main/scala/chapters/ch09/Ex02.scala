48a3c1555dcf1ccf9231c1c621c0627fb1d09326
James Baiera <james.baiera@gmail.com>
2016-03-09 13:53:15 -0500

    Chapter 9 Exercise 2

--- src/main/scala/chapters/ch09/Ex02.scala
+++ src/main/scala/chapters/ch09/Ex02.scala
@@ -1,5 +1,7 @@
 package chapters.ch09
 
+import scala.io.Source
+
 /**
   * Chapter 09, Example 02:
   * Write a Scala program that reads a file with tabs, replaces each tab with spaces
@@ -8,4 +10,9 @@ package chapters.ch09
   */
 object Ex02 extends App {
 
+  val tabStop = 4
+
+  Source.fromFile(getClass.getResource("/chapters/ch09/tabbed.txt").toURI)
+      .flatMap((c) => if (c == '\t') " " * tabStop else Array(c))
+      .foreach(print(_))
 }
