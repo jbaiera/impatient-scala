aaceec7917e7e03b9c0c55cdeb3fc16f0d4cf4c8
James Baiera <james.baiera@gmail.com>
2016-03-10 23:57:33 -0500

    Chapter 9 Exercise 4

--- src/main/scala/chapters/ch09/Ex04.scala
+++ src/main/scala/chapters/ch09/Ex04.scala
@@ -1,5 +1,9 @@
 package chapters.ch09
 
+import chapters.ch09.Ex03._
+
+import scala.io.Source
+
 /**
   * Chapter 09, Example 04:
   * Write a Scala program that reads a text file containing only floating-point
@@ -7,5 +11,34 @@ package chapters.ch09
   * in the file.
   */
 object Ex04 extends App {
+  val sum = Source
+    .fromFile(getClass.getResource("/chapters/ch09/floats.txt").toURI)
+    .getLines()
+    .map(_.toFloat)
+    .sum
 
+  val count = Source
+    .fromFile(getClass.getResource("/chapters/ch09/floats.txt").toURI)
+    .getLines()
+    .size
+
+  val avg = sum / count.toFloat
+
+  val max = Source
+    .fromFile(getClass.getResource("/chapters/ch09/floats.txt").toURI)
+    .getLines()
+    .map(_.toFloat)
+    .max
+
+  val min = Source
+    .fromFile(getClass.getResource("/chapters/ch09/floats.txt").toURI)
+    .getLines()
+    .map(_.toFloat)
+    .min
+
+  println(sum)
+  println(count)
+  println(avg)
+  println(max)
+  println(min)
 }
