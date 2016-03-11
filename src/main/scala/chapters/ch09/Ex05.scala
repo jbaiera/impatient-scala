60681f30e947933bddcefc5c57468d47a72cb577
James Baiera <james.baiera@gmail.com>
2016-03-11 00:38:04 -0500

    Chapter 9 Exercise 5 Some Gradle Fu for handling the output files from exercises.

--- src/main/scala/chapters/ch09/Ex05.scala
+++ src/main/scala/chapters/ch09/Ex05.scala
@@ -1,5 +1,7 @@
 package chapters.ch09
 
+import java.io.PrintWriter
+
 /**
   * Chapter 09, Example 05:
   * Write a Scala program that writes the powers of 2 and their reciprocals to a
@@ -10,5 +12,16 @@ package chapters.ch09
   *   ... ...
   */
 object Ex05 extends App {
+
+  val out = new PrintWriter("out/numbers.txt")
+
+  for (idx <- 1 to 20) {
+    val pow = math.pow(2, idx).toInt
+    val reciprocal = 1 / pow.toDouble
+
+    out.println("%7d %10f".format(pow, reciprocal))
+  }
+
+  out.close()
 
 }
