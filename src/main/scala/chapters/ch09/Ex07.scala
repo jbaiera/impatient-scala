af01b4bdcdb4767411cd48a2029222f7ac15e810
James Baiera <james.baiera@gmail.com>
2016-03-13 12:14:22 -0500

    Chapter 9 Exercise 7

--- src/main/scala/chapters/ch09/Ex07.scala
+++ src/main/scala/chapters/ch09/Ex07.scala
@@ -1,5 +1,7 @@
 package chapters.ch09
 
+import scala.io.Source
+
 /**
   * Chapter 09, Example 07:
   * Write a Scala program that reads a text file and prints all tokens in the file
@@ -7,4 +9,12 @@ package chapters.ch09
   */
 object Ex07 extends App {
 
+  val regex = """\s*\d*\.\d*\s*""".r
+
+  Source
+    .fromFile(getClass.getResource("/chapters/ch09/totesfloats.txt").toURI)
+    .getLines()
+    .map(regex.replaceAllIn(_, " "))
+    .flatMap(_.split(' '))
+    .foreach(println(_))
 }
