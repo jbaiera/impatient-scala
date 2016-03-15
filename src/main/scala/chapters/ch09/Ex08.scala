f3705de0cde6aef5d1df37658231787b98fb6741
James Baiera <james.baiera@gmail.com>
2016-03-14 23:26:58 -0400

    Chapter 9 Exercise 8

--- src/main/scala/chapters/ch09/Ex08.scala
+++ src/main/scala/chapters/ch09/Ex08.scala
@@ -1,10 +1,22 @@
 package chapters.ch09
 
+import java.net.URL
+
+import scala.io.Source
+
 /**
   * Chapter 09, Example 08:
   * Write a Scala program that prints the src attributes of all img tags of a web
   * page. Use regular expressions and groups.
   */
 object Ex08 extends App {
+
+  val imgregex = """<img.*src=".*".*/>""".r
+
+  println(
+    imgregex.findAllMatchIn(
+      Source.fromURL(new URL("http://www.imgur.com")).mkString
+    ).map(_.matched).mkString("\n")
+  )
 
 }
