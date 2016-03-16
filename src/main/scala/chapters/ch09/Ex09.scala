7dc40c5850fc7f47cd107937c505bfbf6b75f1e2
James Baiera <james.baiera@gmail.com>
2016-03-15 23:30:26 -0400

    Chapter 9 Exercise 9

--- src/main/scala/chapters/ch09/Ex09.scala
+++ src/main/scala/chapters/ch09/Ex09.scala
@@ -1,5 +1,7 @@
 package chapters.ch09
 
+import java.io.File
+
 /**
   * Chapter 09, Example 09:
   * Write a Scala program that counts how many files with .class extension are
@@ -7,4 +9,15 @@ package chapters.ch09
   */
 object Ex09 extends App {
 
+  def subdirs(file: File): Iterator[File] = {
+    val children = file.listFiles.filter(_.isDirectory)
+    children.toIterator ++ children.toIterator.flatMap(subdirs)
+  }
+
+  val startingFile = new File(".")
+  val allFiles = Seq(startingFile) ++ subdirs(startingFile)
+
+  val classcount = allFiles.flatMap(_.listFiles.filter((f) => f.isFile)).count(_.getName.endsWith(".class"))
+
+  println(classcount)
 }
