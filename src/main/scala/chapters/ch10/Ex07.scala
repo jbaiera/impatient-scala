a8518d6533f3835103d1e3198942dd86454a788d
James Baiera <james.baiera@gmail.com>
2016-03-21 23:52:07 -0400

    Chapter 10 Exercise 07

--- src/main/scala/chapters/ch10/Ex07.scala
+++ src/main/scala/chapters/ch10/Ex07.scala
@@ -10,4 +10,78 @@ package chapters.ch10
   */
 object Ex07 extends App {
 
+  abstract class Animal(val name: String) {
+    def doWhatYouDo()
+  }
+
+  trait MakesNoise {
+    val name: String
+    def noiseYouMake: String
+
+    def makeNoise() { println(name + " goes " + noiseYouMake) }
+  }
+
+  trait Quacks extends MakesNoise {
+    override def noiseYouMake = "QUACK!"
+  }
+
+  trait Barks extends MakesNoise {
+    override def noiseYouMake = "BARK!"
+  }
+
+  trait isSilent extends MakesNoise {
+    override def noiseYouMake = "..."
+    override def makeNoise() { println(name + "s don't make a sound!") }
+  }
+
+  trait Quietly extends MakesNoise {
+    abstract override def noiseYouMake = super.noiseYouMake + "... stealthily..."
+  }
+
+  trait LikesTo {
+    val name: String
+    def enjoys: String
+    def does: String
+    def likesTo() = println(s"$name enjoys $enjoys")
+    def doWhatYouDo() = println(s"$name $does")
+  }
+
+  trait LikesToRun extends LikesTo {
+    def enjoys = "Running"
+    def does = "Runs"
+  }
+
+  trait LikesToFly extends LikesTo {
+    def enjoys = "Flying"
+    def does = "Flies"
+  }
+
+  trait LikesToStalkHisFoesInTheDarkness extends LikesTo {
+    def enjoys = "Striking from the shadows with deadly precision"
+    def does = "Attacks"
+  }
+
+  val dog = new Animal("Dog") with Barks with LikesToRun
+  dog.makeNoise()
+  dog.likesTo()
+  dog.doWhatYouDo()
+  println()
+
+  val duck = new Animal("Duck") with Quacks with LikesToFly
+  duck.makeNoise()
+  duck.likesTo()
+  duck.doWhatYouDo()
+  println()
+
+  val ninja = new Animal("Ninja") with isSilent with LikesToStalkHisFoesInTheDarkness
+  ninja.makeNoise()
+  ninja.likesTo()
+  ninja.doWhatYouDo()
+  println()
+
+  val ninjaDuck = new Animal("Ninja Duck") with Quacks with Quietly with LikesToStalkHisFoesInTheDarkness
+  ninjaDuck.makeNoise()
+  ninjaDuck.likesTo()
+  ninjaDuck.doWhatYouDo()
+  println()
 }
