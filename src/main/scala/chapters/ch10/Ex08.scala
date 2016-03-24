0c5a79aaf2e20cc8d35563459cb2fc504f7b8514
James Baiera <james.baiera@gmail.com>
2016-03-23 23:24:36 -0400

    Chapter 10 Exercise 08

--- src/main/scala/chapters/ch10/Ex08.scala
+++ src/main/scala/chapters/ch10/Ex08.scala
@@ -1,5 +1,7 @@
 package chapters.ch10
 
+import java.io.{InputStream, ByteArrayInputStream => JByteArrayInputStream}
+
 /**
   * Chapter 10, Example 08:
   * In the java.io library, you add buffering to an input stream with a
@@ -7,5 +9,50 @@ package chapters.ch10
   * override the read method.
   */
 object Ex08 extends App {
+
+  trait Buffered {
+    this: InputStream =>
+    var head = 0
+    var tail = 0
+    var eos = false
+    val buff = new Array[Byte](4086)
+    def fill(): Unit = {
+      if (!eos) {
+        val r = read(buff)
+        tail = 0
+        head = tail + r
+        eos = r < buff.length
+      }
+    }
+    override def read(): Int = {
+      if (eos && tail == head) -1
+      else {
+        if (tail == head) {
+          fill()
+        }
+        val v = buff(tail)
+        tail += 1
+        v
+      }
+    }
+  }
+
+  val bin = new JByteArrayInputStream("Hello World!".getBytes()) with Buffered
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
+  println(bin.read())
 
 }
